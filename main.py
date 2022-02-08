350a13e3aa4d039e680af2e4c5db3642f3ef9a60
rodrialeh01 <2823897250101@ingenieria.usac.edu.gt>
2022-02-07 23:59:27 -0600

    Menu hecho

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import filedialog
+
+opcion = 0
+while(int(opcion)!=3):
+    print('====================================================')
+    print('==                  Ceramica Bot                  ==')
+    print('== Introducción a la Programación y Computación 2 ==')
+    print('==                     MENU                       ==')
+    print('== 1. Cargar Archivo XML                          ==')
+    print('== 2. Procesar Archivo                            ==')
+    print('== 3. Salir                                       ==')
+    print('====================================================')
+    opcion = input('== Elija una opción:                              ==\n')
+
+    if int(opcion) == 1:
+        print('Opcion 1')
+
+    elif int(opcion) == 2:
+        print('Opcion 2')
+
