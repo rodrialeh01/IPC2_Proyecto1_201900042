b9e85c1ee9d4da8a13c5547fc8cb42ca46a72964
rodrialeh01 <2823897250101@ingenieria.usac.edu.gt>
2022-02-11 22:33:27 -0600

    Carga y lectura del archivo exitosa

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
+import tkinter as tk
 from tkinter import filedialog
 
-opcion = 0
-while(int(opcion)!=3):
-    print('====================================================')
-    print('==                  Ceramica Bot                  ==')
-    print('== Introducción a la Programación y Computación 2 ==')
-    print('==                     MENU                       ==')
-    print('== 1. Cargar Archivo XML                          ==')
-    print('== 2. Procesar Archivo                            ==')
-    print('== 3. Salir                                       ==')
-    print('====================================================')
-    opcion = input('== Elija una opción:                              ==\n')
+#ABRIENDO EL ARCHIVO Y VER SU CONTENIDO
+def AbrirArchivo(ruta):
+    if ruta !="":
+        archivo = open(ruta,'r')
+        conarchivo = archivo.read()
+        print(conarchivo)
+    else:
+        print('== NO SE CARGÓ NINGUN ARCHIVO                     ==')
 
-    if int(opcion) == 1:
-        print('Opcion 1')
+#OBTENER LA RUTA DEL ARCHIVO
+def ruta():
+    root = tk.Tk()
+    root.withdraw()
+    ruta =  filedialog.askopenfilename(title='Cargar Archivo', filetypes = (("Text files", "*.xml*"), ("all files", "*.*")))
+    return ruta
 
-    elif int(opcion) == 2:
-        print('Opcion 2')
+#MENU PRINCIPAL
+def menu():
+    opcion = 0
+    Ruta = ""
+    while opcion!=3:
+        print('====================================================')
+        print('==                  CERAMICA-BOT                  ==')
+        print('== Introducción a la Programación y Computación 2 ==')
+        print('==                     MENU                       ==')
+        print('== 1. Cargar Archivo XML                          ==')
+        print('== 2. Procesar Archivo                            ==')
+        print('== 3. Salir                                       ==')
+        print('====================================================')
+        try:
+            opcion = int(input('== Elija una opción:                              ==\n>'))
+            if opcion == 1:
+                try:
+                    Ruta = ruta()
+                    if Ruta !="":
+                        print('== Ruta: ', str(Ruta))
+                        print('== EL ARCHIVO SE CARGO CON EXITO                  ==')
+                    else:
+                        print('== NO SE CARGÓ NINGUN ARCHIVO                     ==')
+                except:
+                    print('== NO SE PUDO CARGAR EL ARCHIVO                   ==')
+            elif opcion == 2:
+                AbrirArchivo(Ruta)
+            elif opcion == 3:
+                print('== ADIOS, VUELVE PRONTO                           ==')
+            else:
+                print('\n== OPCION INVALIDA                                ==')
+        except:
+            print('\n== OPCION INVALIDA                                ==')
 
+menu()
